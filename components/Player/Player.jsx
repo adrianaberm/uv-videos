1150844ef8bed74a7d006c2557e3d002a377533b
Adriana Bermudez <adriana.berm@gmail.com>
2021-04-19 20:17:32 -0500

    Adding video formats

--- components/Player/Player.jsx
+++ components/Player/Player.jsx
@@ -13,7 +13,6 @@ const Player = ({
             url={selectedVideo ? selectedVideo : "https://firebasestorage.googleapis.com/v0/b/uv-videos.appspot.com/o/2-ROSA_v3.mp4?alt=media&token=f880ff57-b3c2-400c-931b-4cf93a12de70"}
             width="100%"
             height="100%"
-            type="video/mp4" 
             playing
             controls/>
     </PlayerWrapper>   
